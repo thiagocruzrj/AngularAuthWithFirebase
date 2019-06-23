f77398063e25c73c1e42ee498881cd9957634608
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2019-06-23 14:58:58 -0300

    Components + Fireauth

--- AngularAuthWithFirebase/src/app/app.module.ts
+++ AngularAuthWithFirebase/src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { EmailComponent } from './email/email.component';
+import { SignupComponent } from './signup/signup.component';
+import { MembersComponent } from './members/members.component';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyBYyEEUEHaJktin7dmX_vfk6I3EBHSCiN0",
+    authDomain: "angularauth-ea3e4.firebaseapp.com",
+    databaseURL: "https://angularauth-ea3e4.firebaseio.com",
+    projectId: "angularauth-ea3e4",
+    storageBucket: "",
+    messagingSenderId: "840996497315",
+    appId: "1:840996497315:web:c64cd71b01df4dc4"
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EmailComponent,
+    SignupComponent,
+    MembersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
